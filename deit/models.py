b2aeab73ed79181976cee858be9d946b5ef1d7a9
Chengyue Gong <cygong@pku.edu.cn>
2021-04-06 13:19:05 -0500

    Update models.py

--- deit/models.py
+++ deit/models.py
@@ -6,10 +6,83 @@ from functools import partial
 import torch.nn.functional as F
 
 # from timm.models.vision_transformer import VisionTransformer, DistilledVisionTransformer, _cfg
-from timm.models.vision_transformer import _cfg, HybridEmbed, PatchEmbed, Block
+from timm.models.vision_transformer import _cfg, HybridEmbed, PatchEmbed
 from timm.models.registry import register_model
 from timm.models.layers import trunc_normal_
 
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+    
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = 0. # AttentionDrop()
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.bias_proj = nn.Sequential(nn.Linear(head_dim * 2, head_dim // 2), nn.ReLU(inplace=True), nn.Linear(head_dim // 2, 3))
+
+    def forward(self, x, prev_attn):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        bias = self.bias_proj( torch.cat((q, k), dim=-1).reshape(-1, C // self.num_heads * 2) )
+        bias = bias.reshape(B, self.num_heads, N, 3)
+
+        cur_attn = attn.softmax(dim=-1)
+        if prev_attn is not None:
+            attn = cur_attn * (1. + bias[:,:,:,:1].abs()) + prev_attn * (.0 + bias[:,:,:,1:2].abs()) + torch.ones_like(cur_attn) / cur_attn.shape[-1] * (-bias[:,:,:,2:].abs())
+        else:
+            attn = cur_attn * (1 + bias.mean() * 0.)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x, cur_attn
+
+class Block(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        
+    def forward(self, x, prev_attn):
+        post_x, attn = self.attn(self.norm1(x), prev_attn)
+        x = x + self.drop_path(post_x)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x, attn
+    
 class VisionTransformer(nn.Module):
     """ Vision Transformer
 
@@ -112,9 +185,10 @@ class VisionTransformer(nn.Module):
         x = torch.cat((cls_tokens, x), dim=1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
-
+        
+        prev_attn = None
         for blk in self.blocks:
-            x = blk(x)
+            x, prev_attn = blk(x, prev_attn)
 
         if self.training: 
             patches = self.norm(x)[:, 1:]
