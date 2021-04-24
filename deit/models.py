b1ae7933ff182e9141ae483f5f19048216e0aeb9
Chengyue Gong <cygong@pku.edu.cn>
2021-04-24 13:59:57 -0500

    Update models.py

--- deit/models.py
+++ deit/models.py
@@ -8,98 +8,23 @@ from timm.models.vision_transformer import VisionTransformer, DistilledVisionTra
 from timm.models.registry import register_model
 from timm.models.layers import trunc_normal_
 
-'''
-class DistilledVisionTransformer(VisionTransformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dist_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
-        num_patches = self.patch_embed.num_patches
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 2, self.embed_dim))
-        self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if self.num_classes > 0 else nn.Identity()
-
-        trunc_normal_(self.dist_token, std=.02)
-        trunc_normal_(self.pos_embed, std=.02)
-        self.head_dist.apply(self._init_weights)
-
-    def forward_features(self, x):
-        # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
-        # with slight modifications to add the dist_token
-        B = x.shape[0]
-        x = self.patch_embed(x)
-
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-        dist_token = self.dist_token.expand(B, -1, -1)
-        x = torch.cat((cls_tokens, dist_token, x), dim=1)
-
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
-
-        for blk in self.blocks:
-            x = blk(x)
-
-        x = self.norm(x)
-        return x[:, 0], x[:, 1]
-
-    def forward(self, x):
-        x, x_dist = self.forward_features(x)
-        x = self.head(x)
-        x_dist = self.head_dist(x_dist)
-        if self.training:
-            return x, x_dist
-        else:
-            # during inference, return the average of both classifier predictions
-            return (x + x_dist) / 2
-'''
-
 @register_model
-def deit_downsampletiny_patch8_224(pretrained=False, **kwargs):
+def deit_large_patch16_224(pretrained=False, **kwargs):
     model = VisionTransformer(
-            patch_size=8, embed_dim=96, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+            img_size=224, patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
             norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
     return model
 
+
 @register_model
-def deit_downsampleextremetiny_patch5_224(pretrained=False, **kwargs):
+def deit_large_patch16_384(pretrained=False, **kwargs):
     model = VisionTransformer(
-            patch_size=5, embed_dim=48, depth=14, num_heads=3, mlp_ratio=4, qkv_bias=True,
+            img_size=384, patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
             norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
     return model
 
-@register_model
-def deit_downsampletiny_patch5_224(pretrained=False, **kwargs):
-    model = VisionTransformer(
-            patch_size=5, embed_dim=64, depth=14, num_heads=2, mlp_ratio=4, qkv_bias=True,
-            norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    model.default_cfg = _cfg()
-    return model
-
-'''
-@register_model
-def deit_downsamplesmall_patch8_224(pretrained=False, **kwargs):
-    model = VisionTransformer(
-            patch_size=8, embed_dim=144, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
-            norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    model.default_cfg = _cfg()
-    return model
-'''
-
-@register_model
-def deit_downsampletiny_patch12_224(pretrained=False, **kwargs):
-    model = VisionTransformer(
-            patch_size=12, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
-            norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    model.default_cfg = _cfg()
-    return model
-
-@register_model
-def deit_downsampleextremetiny_patch12_224(pretrained=False, **kwargs):
-    model = VisionTransformer(
-            patch_size=12, embed_dim=96, depth=16, num_heads=3, mlp_ratio=4, qkv_bias=True,
-            norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    model.default_cfg = _cfg()
-    return model
 
 @register_model
 def deit_downsamplesmall_patch8_224(pretrained=False, **kwargs):
