73b3c4aff0468a636de443844c788a49aee491ec
Chengyue Gong <cygong@pku.edu.cn>
2021-04-23 23:12:15 -0500

    Update datasets.py

--- deit/datasets.py
+++ deit/datasets.py
@@ -98,7 +98,10 @@ def build_transform(is_train, args):
 
     t = []
     if resize_im:
-        size = int((256 / 224) * args.input_size)
+        if args.input_size > 300:
+            size = args.input_size
+        else:
+            size = int((256 / 224) * args.input_size)
         t.append(
             transforms.Resize(size, interpolation=3),  # to maintain same ratio w.r.t. 224 images
         )
