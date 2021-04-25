ebed9bd6f1c215019e3a05bb1d6113a7b0a17be5
Chengyue Gong <cygong@pku.edu.cn>
2021-04-24 22:28:40 -0500

    Update train.sh

--- deit/train.sh
+++ deit/train.sh
@@ -7,7 +7,7 @@ pip install --upgrade .
 cd deit
 export OMP_NUM_THREADS=4
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=1513 --use_env main.py --model deit_small_patch16_224 --batch-size 150 --num_workers 8 --data-path /scratch/cluster/dilin/datasets/imagenet  --output_dir /scratch/cluster/cygong/vision_transformer_small --mixup 0.01 --mixup-switch-prob 0.0  # contrastive
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=1513 --use_env main.py --model deit_small_patch16_224 --batch-size 150 --num_workers 8 --data-path data_path --output_dir output_dir --mixup 0.0 --mixup-switch-prob 0.0 
 
 
 
